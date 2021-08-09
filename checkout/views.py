c279011a9a2bd361441fe6f44bc2ed5d9537ccd0
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-08-09 15:43:32 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JMa7VFwhss2MNNbJ0Vh6jsCQ0J2rpnGruNJwt9uOFvthgZhvz7uBR5NbkrrKEn8wXRDk0wbZs6fRgAK1LZ1Zm6p00tffgkROy',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
