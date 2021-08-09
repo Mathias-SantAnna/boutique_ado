e8d918544beb53e902786073a0405e2e9100af86
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-08-09 13:05:31 +0000

    Added checkout views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
+    path('chaeckout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
